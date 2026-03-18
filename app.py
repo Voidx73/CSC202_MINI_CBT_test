9e5a090d98a37738a2c05a1e5c6c71044a806c69
Voidx73 <waguri02@gmail.com>
2026-03-18 12:05:48 +0100

    Add random comment 2

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from datetime import datetime
 base_dir = os.path.abspath(os.path.dirname(__file__))
 template_dir = os.path.join(base_dir, 'templates')
 
+# Random comment 2: Setting up the Flask app with custom template directory.
+
 app = Flask(__name__, template_folder=template_dir)
 app.secret_key = "csc202_secret_key"
 
